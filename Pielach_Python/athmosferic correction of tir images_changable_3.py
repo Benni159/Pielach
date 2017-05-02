46b4d27b9c8d81104ccc4d2c11b976a295c5577b
Bapperl <bapperl@gmail.com>
2017-05-02 14:13:35 +0200

    Athmosferic correction of UAV flights
    Faster Version of atthmosferic correction

--- Pielach_Python/athmosferic correction of tir images_changable_3.py
+++ Pielach_Python/athmosferic correction of tir images_changable_3.py
@@ -23,11 +23,10 @@ import os
 os.chdir('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Python')
 import glob
 import pandas as pd
-import matplotlib.pyplot as plt
 import cv2
 import pielach_funs as pfs
-
-
+import math
+import matplotlib.pyplot as plt
 main_dir = 'd:\Doktorat\Messungen\Pielach\Daten\Pielach'
 
 
@@ -42,41 +41,41 @@ main_dir = 'd:\Doktorat\Messungen\Pielach\Daten\Pielach'
 #time = 'T17'
 
 #ACTIVATE IF LIST OF RUNS  
-measurements=pd.read_csv(os.path.join(main_dir,'Messungen.csv'),header=0,sep=',')
+measurements = pd.read_csv(os.path.join(main_dir,'Messungen.csv'),header=0,sep=',')
 #measurements=measurements.drop(7)
  
-for q in range(0,len(measurements)):
+for q in range(8,len(measurements)):
     date = measurements.get_value(q,'Day')
     time = measurements.get_value(q,'Time')
 
-    day=(date[-2:])
-    month=(date[-4:-2])
-    year=(date[1:5])
+    day = (date[-2:])
+    month = (date[-4:-2])
+    year = (date[1:5])
     
-    if len(time)==5:
-        minute=time[3:5]
-        hour=time[1:3]
+    if len(time) == 5:
+        minute = time[3:5]
+        hour = time[1:3]
         
-        Datetime=datetime.datetime(int(year),int(month),int(day),int(hour),int(minute),0,0)
+        Datetime = datetime.datetime(int(year),int(month),int(day),int(hour),int(minute),0,0)
         
     else:
-        Datetime=datetime.datetime(int(year),int(month),int(day),int(time[1:]),0,0)
+        Datetime = datetime.datetime(int(year),int(month),int(day),int(time[1:]),0,0)
     
     ### Define Directiories
     csv_dir = os.path.join(main_dir, date, time, 'Thermal', 'Bilder',)
     TIR_dir = os.path.join(csv_dir,'tiff_pseudocolor')
     GPS_offset_dir=os.path.join(main_dir,'SD_Karte_LOG')
     
-    TIRlambda_min= 7.5       # microns
-    TIRlambda_max=13        #microns
-    Temp=290                # approximate Temperature of water surface 
+    TIRlambda_min = 7.5       # microns
+    TIRlambda_max = 13        #microns
+    Temp = 290                # approximate Temperature of water surface in K
     
     
     #Resolution of Tir images
-    x_res=382
-    y_res=288
+    x_res = 382
+    y_res = 288
     #focal length
-    f=  553
+    f =  553
     deltah = 2               #2m offset wiel zwischen drohnenstartpunkt und wasser delta h=2m    : + fürDrohenstart höher als Zeilflaeche
     #flight height offest to be set if fix
 
@@ -123,7 +122,7 @@ for q in range(0,len(measurements)):
     # Load passmann laromre table
     PLWater=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_H20.csv',sep=';', header=0)
     PLco2=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_CO2.csv',sep=';', header=0)
-    
+    PLWater_local=PLWater[(PLWater.Wavelength >=TIRlambda_min) & (PLWater.Wavelength <= TIRlambda_max)]
     
     
     #Load data from weatherstation
@@ -136,17 +135,17 @@ for q in range(0,len(measurements)):
         
     drift =pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Athmosferic correction\Drift\linear gradient.csv', sep = ';', header = 0)
     
-    #Get downwelling athomsferic transmittance
+    #Get downwelling athomsferic transmittance from Low Tran
     L_down=pd.read_csv(os.path.join(main_dir,'Auswertungen','Athmosferic correction','Transmittance','Lowtran_Transmissivity_Path_to_space_Midaltitudesummer.csv'))
-    L_down_transmit=np.mean(L_down['transmission'])
+   
     
     
     #%% Get relevant data from tables^^
     
-    if q > 3:
+    if q > 2:
     
         RH = Weatherstation.loc[pd.to_datetime(Datetime),'v02humidity']/100
-        ta = Weatherstation.loc[pd.to_datetime(Datetime),'v02humidity']/100 
+        ta = Weatherstation.loc[pd.to_datetime(Datetime),'v01temp']
     elif q == 1:
         RH = 0.52
         ta = 25.1
@@ -156,6 +155,9 @@ for q in range(0,len(measurements)):
     elif q == 2:
         RH = 0.65
         ta = 21.2
+        
+    
+   
     #%%Define paths of otuput tables
         
     #create path:
@@ -189,7 +191,30 @@ for q in range(0,len(measurements)):
         os.makedirs(temp_corr_dir_drift)          
                 
      
-    #%% Calculate data for every image    
+
+
+
+ #%% Calculate refraction index for water and weghted values
+    
+    h = 6.626*10**-34 #(J*s    =Planck constant)
+    c = 299792458     # (speed of light m/s)    
+
+    # Emissivity wehgthted for water
+    refraction_trans = (2*math.pi*h*c**2/((PLWater_local['Wavelength'])*10**-6)**5)/(math.e**((0.0144)/(PLWater_local['Wavelength']*10**-6)/(Temp))-1)    #spektrale spez.Ausstahrlung 
+                       
+    
+    #Calcualte weihgt for air tmerpareture wavelegth intensity ta
+    fuzzy_weight = np.linspace(0,1,len(PLWater_local)/2)
+    fuzzy_weight = np.append(fuzzy_weight,fuzzy_weight[::-1])
+    weight_ta=(2*math.pi*h*c**2/((PLWater_local['Wavelength'])*10**-6)**5)/(math.e**((0.0144)/(PLWater_local['Wavelength']*10**-6)/(ta+273.15))-1)
+    PLWater_avg_weighted_fuzzy_ta_Lup=np.average(PLWater_local,axis=0,weights=(weight_ta*fuzzy_weight))
+    
+    #calculated weighted atmosphere temp intensity
+    weight_ta_Ldown=(2*math.pi*h*c**2/((L_down['wavelength_nm'])*10**-9)**5)/(math.e**((0.0144)/(L_down['wavelength_nm']*10**-9)/(ta+273.15))-1)
+    L_down_transmit=np.average(L_down['transmission'],axis = 0, weights = weight_ta_Ldown)
+    
+      #%% Calculate data for every image      
+    
 
 
     # Get downwelling transmissivity for mit-altitude summer from whole atmosphere averaged over the wavelength spectrum
@@ -246,107 +271,107 @@ for q in range(0,len(measurements)):
         
             '''include pitch and roll in zenith angle'''
         
-            #if camera_angle['#Label'].str.contains(filename).any()==True:
+            if camera_angle['#Label'].str.contains(filename).any()==True:
     
-            yaw=camera_angle.loc[camera_angle['#Label'] == filename,'Yaw_est'].values
-            pitch=camera_angle.loc[camera_angle['#Label'] == filename,'Pitch_est'].values
-            roll=camera_angle.loc[camera_angle['#Label'] == filename,'Roll_est'].values
-            
-            x_mulitplik_layer=np.zeros((y_res,x_res))
-            x_mulitplik_layer[0:(y_res-1),0:(int((x_res)/2))]=-1
-            x_mulitplik_layer[0:(y_res-1),(int((x_res-1)/2))+1:]=1               
-            x_angleCorr=x_angle*x_mulitplik_layer+roll
-            
-            y_mulitplik_layer=np.zeros((y_res,x_res,))
-            y_mulitplik_layer[ :int((y_res/2)),:]=-1
-            y_mulitplik_layer[int((y_res/2)):,:]=1               
-            y_angleCorr=y_angle*y_mulitplik_layer-pitch
-            
-            
-            xy_angle_corr=np.zeros((y_res,x_res))
-            xy_angle_corr=np.degrees((np.sqrt((z*np.tan(np.deg2rad(abs(x_angleCorr))))**2+
-                    (z*np.tan(np.deg2rad(abs(y_angleCorr))))**2))/z)   
-        
-            xy_dist_corr=z/np.cos(np.deg2rad(xy_angle_corr))     
-                   
-            cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Angle_'+filename),xy_angle_corr)
-            cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Path_'+filename),xy_dist_corr)
-            
-            # Turn interactive plotting off
+                yaw=camera_angle.loc[camera_angle['#Label'] == filename,'Yaw_est'].values
+                pitch=camera_angle.loc[camera_angle['#Label'] == filename,'Pitch_est'].values
+                roll=camera_angle.loc[camera_angle['#Label'] == filename,'Roll_est'].values
                 
-            plt.ioff() 
-            fig = plt.figure()
-            plt.savefig(os.path.join(angle_pseudocolor_dir,'PseudocolorAngle'+filename))
-            plt.matshow(xy_angle_corr)
-            plt.close('all')
-                           
-            percent=round((m/len(TIR_files)*100),1)
-            print (filename + ' written    '+str(percent) +' %' )
-            
-            
-        #   CALCULATE emissivity    
-        
-            refraction_index = pfs.refractive_index(TIRlambda_min,TIRlambda_max,Temp)    
-            emissivity_corr=np.zeros((y_res,x_res))
-            
-            for i in range(0,y_res):
-               for j in range(0,x_res):
-                   emissivity_corr[i][j] = pfs.emissivity_angle(xy_angle_corr[i,j],refraction_index)
-                   
-            cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Emissivity_'+filename),emissivity_corr)
-            
-            print('            Emissivity finished')
-            
-        #   CALCULATE transmissivity ath pathlengh z
-        
-            transmissivity_corr=np.zeros((y_res,x_res))
-            
-        
-            for i in range(0,y_res):
-               for j in range(0,x_res):
-                   dist=xy_dist_corr[i,j]
-                   transmissivity_corr[i][j] = pfs.transmissivity(RH,Temp,dist,TIRlambda_min,TIRlambda_max,PLWater)
-                   
-            np.savetxt(os.path.join(csv_dir,'transmissivity','Transmissivity_weights_sensisitivy_triangle'+(filename[:-11]+'csv')),transmissivity_corr,delimiter=';',fmt='%.4f')
-                   
-            print('            Transmissivity finished')
-            
-                        
-        #   CALCULATE PATH RADIANCE
-        
-            Path_Radiance=(1-transmissivity_corr)*ta**4
-            
-        #'''Recalculate image temperature´'''
-            
-              #get image csv
-            csv_name=filename[:-12]+'.csv'
-            csv_file=np.zeros((y_res,x_res))
-            csv_file=pd.read_csv(os.path.join(csv_dir,csv_name),sep= ';',header=None) 
-            csv_file=csv_file.dropna(axis=1, how='all')
-                                     
-        
-                #calculate corrected temp
-            temp_CORR=((((csv_file+273.15)**4-Path_Radiance)/transmissivity_corr-((1-emissivity_corr)*(1-L_down_transmit)*(ta+273.15)**4))/emissivity_corr)**0.25
-            temp_CORR_inC = temp_CORR-273.15
+                x_mulitplik_layer=np.zeros((y_res,x_res))
+                x_mulitplik_layer[0:(y_res-1),0:(int((x_res)/2))]=-1
+                x_mulitplik_layer[0:(y_res-1),(int((x_res-1)/2))+1:]=1               
+                x_angleCorr=x_angle*x_mulitplik_layer+roll
                 
-            diff_temp=(temp_CORR-csv_file)-273.15
+                y_mulitplik_layer=np.zeros((y_res,x_res,))
+                y_mulitplik_layer[ :int((y_res/2)),:]=-1
+                y_mulitplik_layer[int((y_res/2)):,:]=1               
+                y_angleCorr=y_angle*y_mulitplik_layer-pitch
+                
+                
+                xy_angle_corr=np.zeros((y_res,x_res))
+                xy_angle_corr=np.degrees((np.sqrt((z*np.tan(np.deg2rad(abs(x_angleCorr))))**2+
+                        (z*np.tan(np.deg2rad(abs(y_angleCorr))))**2))/z)   
+            
+                xy_dist_corr=z/np.cos(np.deg2rad(xy_angle_corr))     
+                       
+                cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Angle_'+filename),xy_angle_corr)
+                cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Path_'+filename),xy_dist_corr)
+                
+                # Turn interactive plotting off
+                    
+                #plt.ioff() 
+               # fig = plt.figure()
+                #plt.savefig(os.path.join(angle_pseudocolor_dir,'PseudocolorAngle'+filename))
+                #plt.matshow(xy_angle_corr)
+                #plt.close('all')
+                               
+                percent=round((m/len(TIR_files)*100),1)
+                print (filename + ' written    '+str(percent) +' %' )
+                
+                
+            #   CALCULATE emissivity    
+            
+                refraction_index = pfs.refractive_index(TIRlambda_min,TIRlambda_max,Temp)    
+                emissivity_corr=np.zeros((y_res,x_res))
+                
+                for i in range(0,y_res):
+                   for j in range(0,x_res):
+                       emissivity_corr[i][j] = pfs.emissivity_angle(xy_angle_corr[i,j],refraction_index)
+                       
+                cv2.imwrite(os.path.join(csv_dir,'Pixelangle','Emissivity_'+filename),emissivity_corr)
+                
+                print('            Emissivity finished')
+                
+            #   CALCULATE transmissivity ath pathlengh z
+            
+                transmissivity_corr=np.zeros((y_res,x_res))
+                
+            
+                for i in range(0,y_res):
+                   for j in range(0,x_res):
+                       dist=xy_dist_corr[i,j]
+                       transmissivity_corr[i][j] = pfs.transmissivity(RH,ta,dist,TIRlambda_min,TIRlambda_max,PLWater_avg_weighted_fuzzy_ta_Lup,refraction_trans)
+                       
+                np.savetxt(os.path.join(csv_dir,'transmissivity','Transmissivity_weights_sensisitivy_triangle'+(filename[:-11]+'.csv')),transmissivity_corr,delimiter=';',fmt='%.4f')
+                       
+                print('            Transmissivity finished')
+                
+                            
+            #   CALCULATE PATH RADIANCE
+            
+                Path_Radiance=(1-transmissivity_corr)*ta**4
+                
+            #'''Recalculate image temperature´'''
+                
+                  #get image csv
+                csv_name=filename[:-12]+'.csv'
+                csv_file=np.zeros((y_res,x_res))
+                csv_file=pd.read_csv(os.path.join(csv_dir,csv_name),sep= ';',header=None) 
+                csv_file=csv_file.dropna(axis=1, how='all')
+                                         
+            
+                    #calculate corrected temp
+                temp_CORR=((((csv_file+273.15)**4-Path_Radiance)/transmissivity_corr-((1-emissivity_corr)*(1-L_down_transmit)*(ta+273.15)**4))/emissivity_corr)**0.25
+                temp_CORR_inC = temp_CORR-273.15
+                    
+                diff_temp=(temp_CORR-csv_file)-273.15
+                
+                
+                temp_CORR_inC.to_csv(os.path.join(csv_dir,'temp_corr' ,'temp_corr_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
+                diff_temp.to_csv(os.path.join(csv_dir,'temp_corr' ,'difftemp_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
+                #cv2.imwrite(os.path.join(csv_dir,'temp_corr' ,'temp_corr'+filename), temp_CORR_inC)
+                #cv2.imwrite(os.path.join(csv_dir,'temp_corr' ,'difftemp'+filename), diff_temp)
+                
+                
+            #Add linear temperature dirft
+                
+                
+                drift_vect=np.linspace((len(TIR_files)*abs(drift.get_value(q,'Gradient linear'))),0,num=(len(TIR_files)))
+                temp_CORR_in_C_drift=temp_CORR_inC-drift_vect[m-1]
+                temp_CORR_in_C_drift.to_csv(os.path.join(temp_corr_dir_drift ,'temp_corr_drift_corr_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
             
             
-            temp_CORR_inC.to_csv(os.path.join(csv_dir,'temp_corr' ,'temp_corr_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
-            diff_temp.to_csv(os.path.join(csv_dir,'temp_corr' ,'difftemp_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
-            #cv2.imwrite(os.path.join(csv_dir,'temp_corr' ,'temp_corr'+filename), temp_CORR_inC)
-            #cv2.imwrite(os.path.join(csv_dir,'temp_corr' ,'difftemp'+filename), diff_temp)
-            
-            
-        #Add linear temperature dirft
-            
-            
-            drift_vect=np.linspace((len(TIR_files)*abs(drift.get_value(q,'Gradient linear'))),0,num=(len(TIR_files)))
-            temp_CORR_in_C_drift=temp_CORR_inC-drift_vect[m-1]
-            temp_CORR_in_C_drift.to_csv(os.path.join(temp_corr_dir_drift ,'temp_corr_drift_corr_'+csv_name),sep=';',header=None, index=None, float_format='%.2f')
-        
-        
-            #m=m+1
+                #m=m+1
         
     pfs.csv2tiffs(os.path.join(csv_dir,'temp_corr'),os.path.join(csv_dir,'temp_corr' ))
     pfs.csv2tiffs(os.path.join(temp_corr_dir_drift),os.path.join(temp_corr_dir_drift ))
--- Pielach_Python/pielach_funs.py
+++ Pielach_Python/pielach_funs.py
@@ -57,10 +57,11 @@ def refractive_index (lambda_min,lambda_max,Temp): #in microns and °K        CA
 
 
     refraction=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\water_refractive index_wavelngth_at 25°C.txt',sep= ',')
-    refraction ['Ml']=(2*math.pi*h*c**2/((refraction['wl'])*10**-6)**5)/(math.e**((0.0144)/(refraction['wl']*10**-6)/(Temp+273.15)-1))     #spektrale spez.Ausstahrlung
+    refraction ['Ml']=(2*math.pi*h*c**2/((refraction['wl'])*10**-6)**5)/(math.e**((0.0144)/(refraction['wl']*10**-6)/(Temp))-1)     #spektrale spez.Ausstahrlung
 
-    refraction=refraction[(refraction['wl'] >=lambda_min) & (refraction['wl'] <= lambda_max)]   #filter Vaukles in wavelength
+    refraction=refraction[(refraction['wl'] >=lambda_min) & (refraction['wl'] <= lambda_max)]   #filter Values in wavelength
 
+    #Calcualte weghted refaction
     refractive_index=sum(refraction.n*refraction.Ml)/sum(refraction.Ml)
     return (refractive_index)
 
@@ -87,13 +88,13 @@ def emissivity_angle (incident_angle,refractive_index):     # get the emissivity
     return emissivity
 
 
-def transmissivity (RH,Temp,distance,lambda_min,lambda_max,PLWater):
+def transmissivity (RH,ta,distance,lambda_min,lambda_max,PLWater_avg_weighted_fuzzy,refraction):
 
     import numpy as np
     #import pandas as pd
     import math
 
-
+    ta_inK=ta+273.15
 
     #constants
     h = 6.626*10**-34 #(J*s    =Planck constant)
@@ -104,33 +105,33 @@ def transmissivity (RH,Temp,distance,lambda_min,lambda_max,PLWater):
     Rv= 461.4               # spec. gas constant for water
     #Temp_C=np.linspace(0,40,401)
     #Magnus formula to relate satuartion vapor pressure and temperature
-    esw = (10**(22.5518+(-2937.4/(Temp)))*(Temp)**(-4.9283))*10    # in mbar
-    H1 = 100/Rv/(Temp)*esw
+    esw = (10**(22.5518+(-2937.4/(ta_inK)))*(ta_inK)**(-4.9283))*10    # in mbar
+    H1 = 100/Rv/(ta_inK)*esw
     Vh20_RH100 =  H1*1000  #mm/km
     h_RH=RH*Vh20_RH100*distance*10**-3
 
-    #load Passmann Larmore Tables (load this file in original file to be faster)
-    #PLWater=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_H20.csv',sep=';', header=0)
-    #PLco2=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_CO2.csv',sep=';', header=0)
-
-    # filter wavelengths
-    PLWater_local=PLWater[(PLWater.Wavelength >=lambda_min) & (PLWater.Wavelength <= lambda_max)]
+    #load this tables to be faster (load this file in original file to be faster)
+        #PLWater=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_H20.csv',sep=';', header=0)
+        #PLco2=pd.read_csv('d:\Doktorat\Messungen\Pielach\Daten\Pielach\Auswertungen\Other input data\Passmore Larmore Table_CO2.csv',sep=';', header=0)
     
-    #PLco2=PLco2[(PLco2.Wavelength >=lambda_min) & (PLco2.Wavelength <= lambda_max)]
-
-    #Arthimetic mean over wavelength spektrum
-    #PLWater_avg=np.mean(PLWater)
+        # filter wavelengths
+        #PLWater_local=PLWater[(PLWater.Wavelength >=lambda_min) & (PLWater.Wavelength <= lambda_max)]
+        
+        #PLco2=PLco2[(PLco2.Wavelength >=lambda_min) & (PLco2.Wavelength <= lambda_max)]
     
-    
-    ##ACTIVATE IF JUST WEIGTHED OVER WVAELENGTH: Weihted Average over wl spetrum with  P instensites
-    refraction =(2*math.pi*h*c**2/((PLWater_local['Wavelength'])*10**-6)**5)/(math.e**((0.0144)/(PLWater_local['Wavelength']*10**-6)/(Temp+273.15)-1))     #spektrale spez.Ausstahrlung
-    #PLWater_avg_weigthed=np.average(PLWater_local,axis=0, weights=refraction)
-    #PLWater_avg_weighted_fuzzy=PLWater_avg_weigthed
-    
-    #ACTIVATE IF WEIGTHED OVER WVAELENGTH AND CAMERA SENSITIVTYWeighted average combined with traingular sensitivity of camera over the spectrum
-    fuzzy_weight = np.linspace(0,1,len(PLWater_local)/2)
-    fuzzy_weight=np.append(fuzzy_weight,fuzzy_weight[::-1])
-    PLWater_avg_weighted_fuzzy=np.average(PLWater_local,axis=0,weights=(refraction*fuzzy_weight))
+        #Arthimetic mean over wavelength spektrum
+        #PLWater_avg=np.mean(PLWater)
+        
+        
+        ##ACTIVATE IF JUST WEIGTHED OVER WVAELENGTH: Weihted Average over wl spetrum with  P instensites
+        #refraction =(2*math.pi*h*c**2/((PLWater_local['Wavelength'])*10**-6)**5)/(math.e**((0.0144)/(PLWater_local['Wavelength']*10**-6)/(Temp+273.15)-1))     #spektrale spez.Ausstahrlung
+        #PLWater_avg_weigthed=np.average(PLWater_local,axis=0, weights=refraction)
+        #PLWater_avg_weighted_fuzzy=PLWater_avg_weigthed
+        
+        #ACTIVATE IF WEIGTHED OVER WVAELENGTH AND CAMERA SENSITIVTYWeighted average combined with traingular sensitivity of camera over the spectrum
+        #fuzzy_weight = np.linspace(0,1,len(PLWater_local)/2)
+       #fuzzy_weight=np.append(fuzzy_weight,fuzzy_weight[::-1])
+        #PLWater_avg_weighted_fuzzy=np.average(PLWater_local,axis=0,weights=(refraction*fuzzy_weight))
 
     # get Specific value
     column_h=np.array([0,0.2,0.5,1,2,5,10,20,50])
@@ -175,3 +176,76 @@ def csv2tiffs(csv_dir, tiff_dir, start = 0, stop = []):
         img = np.array(img * 100, dtype = np.uint16)
         cv2.imwrite(os.path.join(tiff_dir, os.path.splitext(f)[0] + '.tiff'), img)
         print('saving: %s' % f)
+        
+        
+        
+        
+
+
+
+def my_legend(axis = None):    # In line labelling
+    
+    
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from scipy import ndimage
+
+    if axis == None:
+        axis = plt.gca()
+
+    N = 32
+    Nlines = len(axis.lines)
+    print (Nlines)
+
+    xmin, xmax = axis.get_xlim()
+    ymin, ymax = axis.get_ylim()
+
+    # the 'point of presence' matrix
+    pop = np.zeros((Nlines, N, N), dtype=np.float)    
+
+    for l in range(Nlines):
+        # get xy data and scale it to the NxN squares
+        xy = axis.lines[l].get_xydata()
+        xy = (xy - [xmin,ymin]) / ([xmax-xmin, ymax-ymin]) * N
+        xy = xy.astype(np.int32)
+        # mask stuff outside plot        
+        mask = (xy[:,0] >= 0) & (xy[:,0] < N) & (xy[:,1] >= 0) & (xy[:,1] < N)
+        xy = xy[mask]
+        # add to pop
+        for p in xy:
+            pop[l][tuple(p)] = 1.0
+
+    # find whitespace, nice place for labels
+    ws = 1.0 - (np.sum(pop, axis=0) > 0) * 1.0 
+    # don't use the borders
+    ws[:,0]   = 0
+    ws[:,N-1] = 0
+    ws[0,:]   = 0  
+    ws[N-1,:] = 0  
+
+    # blur the pop's
+    for l in range(Nlines):
+        pop[l] = ndimage.gaussian_filter(pop[l], sigma=N/5)
+
+    for l in range(Nlines):
+        # positive weights for current line, negative weight for others....
+        w = -0.3 * np.ones(Nlines, dtype=np.float)
+        w[l] = 0.5
+
+        # calculate a field         
+        p = ws + np.sum(w[:, np.newaxis, np.newaxis] * pop, axis=0)
+        plt.figure()
+        plt.imshow(p, interpolation='nearest')
+        plt.title(axis.lines[l].get_label())
+
+        pos = np.argmax(p)  # note, argmax flattens the array first 
+        best_x, best_y =  (pos / N, pos % N) 
+        x = xmin + (xmax-xmin) * best_x / N       
+        y = ymin + (ymax-ymin) * best_y / N       
+
+
+        axis.text(x, y, axis.lines[l].get_label(), 
+                  horizontalalignment='center',
+                  verticalalignment='center')
+
+
